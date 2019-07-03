1402f0c32f06dee23437a32227b8ffac8f1fbe04
NarenderChary892 <narender.uppla@gmail.com>
2019-07-03 19:13:50 +0530

    JumpSearch

--- Searching/JumpSearch.cs
+++ Searching/JumpSearch.cs
@@ -8,8 +8,12 @@ namespace Algorithms.Searching {
 
       /*
        What is the optimal block size to be skipped?
-            In the worst case, we have to do n/m jumps and if the last checked value is greater than the element to be searched for, we perform m-1 comparisons more for linear search. 
-            Therefore the total number of comparisons in the worst case will be ((n/m) + m-1). The value of the function ((n/m) + m-1) will be minimum when m = √n. Therefore, the best step size is m = √n.
+            In the worst case, we have to do n/m jumps and if the last checked value is greater than the element to be searched for, 
+            we perform m-1 comparisons more for linear search. 
+
+            Therefore the total number of comparisons in the worst case will be ((n/m) + m-1). 
+            The value of the function ((n/m) + m-1) will be minimum when m = √n. 
+            Therefore, the best step size is m = √n.
        
        Time Complexity : O(√n)
        Auxiliary Space : O(1)
@@ -19,7 +23,9 @@ namespace Algorithms.Searching {
        -> Works only sorted arrays.
        -> The optimal size of a block to be jumped is (√ n). This makes the time complexity of Jump Search O(√ n).
        -> The time complexity of Jump Search is between Linear Search ( ( O(n) ) and Binary Search ( O (Log n) ).
-       -> Binary Search is better than Jump Search, but Jump search has an advantage that we traverse back only once (Binary Search may require up to O(Log n) jumps, consider a situation where the element to be search is the smallest element or smaller than the smallest). So in a systems where jumping back is costly, we use Jump Search.
+       -> Binary Search is better than Jump Search, but Jump search has an advantage that we traverse back only once 
+            (Binary Search may require up to O(Log n) jumps, consider a situation where the element to be search is the 
+            smallest element or smaller than the smallest). So in a systems where jumping back is costly, we use Jump Search.
        */
       int[] arr = Array.ConvertAll( Console.ReadLine().Split( ',' ), temp => Convert.ToInt32( temp ) );
       int searchElement = Convert.ToInt32( Console.ReadLine() );
